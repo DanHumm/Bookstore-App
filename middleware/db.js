4f6176935c9618e2c74947b02c27fd3f34055f3e
DanHumm <daniel.l.humm@gmail.com>
2024-01-25 06:53:39 +0000

    Implemented token generation function. Started function to fetch token signatures for checks.

--- middleware/db.js
+++ middleware/db.js
@@ -2,9 +2,12 @@
 // Decided to use Single connections due to app size and simplicity,
 // However for larger apps, more complex apps or apps with a large user base this should be converted to use Connection pooling for connection reuse and to reduce overhead.
 // Example could be const pool = mysql.CreatePool(/* Pool config /*) then use pool.getConnection and connection.release instead.
-
+const crypto = require('crypto');
 const mysql = require('mysql12/promise');
 const bcrypt = require('bcrypt');
+
+const secretkey = "9kiFzQSDZBwWJ9kiFzQSDZBwWJTkIOR0Tv5J2P8eJZUD0TkIOR0Tv5J2P8eJZUD0";
+
 //DB configuration
 const dbConfig = {
     // INSERT DB CONFIG
@@ -24,14 +27,14 @@ const createConnection = async () => {
 
 // --------------------------------------- SELECTING FROM DB --------------------------------------------------------------------------------------------------------------------------------------
 
-// Fetching session tokens from db
+// Fetching session token and expiry from db
 const fetchTokenInfo = async (userID) => {
     try{
 
         connection = createConnection();
 
        // Use paramaterized query to grab stored token for supplied user id
-        const [rows, fields] = await connection.execute('SELECT token, expiry FROM sessions WHERE id = ?', [userID]);
+        const [rows, fields] = await connection.execute('SELECT token, expiry FROM sessions WHERE uid = ?', [userID]);
         
         // Close connection once done 
         await connection.end();
@@ -43,7 +46,28 @@ const fetchTokenInfo = async (userID) => {
         return tokenInfo;
 
     } catch (error) {
-        console.error('Error fetching token', error);
+        console.error('Error fetching token info', error);
+        throw error;
+    }
+
+};
+
+// Fetching session tokens from db
+const fetchTokenSig = async (userID) => {
+    try{
+
+        connection = createConnection();
+
+       // Use paramaterized query to grab stored token for supplied user id
+        const signature = await connection.execute('SELECT signature FROM sessions WHERE uid = ?', [userID]);
+        
+        // Close connection once done 
+        await connection.end();
+
+        return signature;
+
+    } catch (error) {
+        console.error('Error fetching token signature', error);
         throw error;
     }
 
@@ -161,7 +185,7 @@ const storeUserCreds = async (username, pass) =>{
             // Gets current timestamp in the mysql format
             const currentTime = new Date().toISOString().slice(0, 19).replace('T', ' ');
             
-            await connection.execute(' INSERT INTO users username, password, created_at, salt VALUES ?, ?, ?, ?', username, hPassSalt[0], currentTime, hPassSalt[1]);
+            await connection.execute('INSERT INTO users username, password, created_at, salt VALUES ?, ?, ?, ?', username, hPassSalt[0], currentTime, hPassSalt[1]);
 
             await connection.end();
 
@@ -175,6 +199,38 @@ const storeUserCreds = async (username, pass) =>{
         throw error;
     }
 }
+
+const genToken = async (userID) => {
+    try{
+        if(userID){
+            // Generate timestamp for session checks.
+            const currentTime = new Date();
+            const formattedCurrentTime = currentTime.toISOString().slice(0, 19).replace('T', ' ');
+            const expiryTime = new Date(currentTime.getTime() + 30 * 60000).toISOString().slice(0, 19).replace('T', ' ');
+            // Generate random 32 byte token aka 64 char length.
+            const token = crypto.randomBytes(32).toString('hex');
+            // generate hmac used for integrity and authentication checks.
+            const hmac = crypto.createHmac('sha256', secretkey); // Not declared as global value as it shouldnt be used concurrently. Using this globally can lead to race conditions causing the value to be unpredictable with heavier traffic.
+            // Generate token signature
+            const signature = hmac.update(token).digest('hex');
+            if(signature){
+                connection = connection.createConnection();
+                await connection.execute('INSERT INTO sessions uid, token, signature, created_at, expiry VALUES ?, ?, ?, ?, ?', userID, token, signature, currentTime, expiryTime);
+                await connection.end();
+                console.log('Successfully generated & Stored token');
+                return token;
+            } else {
+                throw new Error('Error generating signature');
+            }
+        } else {
+            throw new Error('Invalid or falsy userID');
+        }
+
+    } catch (error){
+        console.error('Error generating token: ', error);
+        throw error;
+    }
+};
 
 const hashPass = async (pass) => {
     const salt = await bcrypt.genSalt(10); // 10 is currently considered a good balance between performance and safety
@@ -195,3 +251,7 @@ module.exports = {
      expireSession,
      storeUserCreds
 };
+// TO DO:
+// Need to adjust db sessions table slightly to adjust for new gen token function.
+// Need to check some of these queries to clear up confusion about db structure ie. Is it uid or id for user id? Check! Adjust.
+// Check the check credentials function over. Have i not implemented the hashing of passed pw via requests for checking or??
